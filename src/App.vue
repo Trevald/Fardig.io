3bf188f53ee1ee06fcd286b51b3ec88508f4f6af
Benjamin Holfve <benjamin@trevald.com>
2019-04-15 12:07:51 +0200

    Added some component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,23 @@
 <template>
   <div id="app">
-    <img width="25%" src="./assets/logo.png">
-    <HelloWorld/>
+    <Editor/>
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld";
+import Editor from "./components/Editor";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    Editor
   }
 };
 </script>
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
 }
 </style>
---
+++ src/components/Editor.vue
@@ -0,0 +1,46 @@
+<template>
+<div>
+  <div id="codex-editor"></div>
+</div>
+</template>
+
+<script>
+
+  import EditorJS from "@editorjs/editorjs";
+  import Header from "@editorjs/header";
+  import List from "@editorjs/list";
+  import Checklist from "@editorjs/checklist";
+
+  export default {
+    name: "Editor",
+
+    data() {
+      return {
+        editor: undefined
+      }
+    },
+
+    mounted() {
+
+      this.editor = new EditorJS({
+        autofocus: true,
+        holderId: "codex-editor",
+        tools: {
+          header: {
+            class: Header,
+            inlineToolbar: true,
+            shortcut: 'CMD+SHIFT+H'
+          },
+          checklist: {
+            class: Checklist,
+            inlineToolbar: true
+          },
+        }
+      });
+    }
+
+
+
+  }
+
+</script>
