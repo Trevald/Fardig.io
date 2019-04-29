c41eb9299eba48e66c51863540c7470cdb22fedc
Benjamin Holfve <benjamin@trevald.com>
2019-04-29 18:37:50 +0200

    Added html to Navigation component

---
+++ src/components/NavigationMain.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav class="navigation-bar">
+    <ul>
+      <li>
+        <a href="#">Item 1</a>
+      </li>
+      <li class="active">
+        <a href="#">Item 2</a>
+      </li>
+      <li>
+        <a href="#">Item 3</a>
+      </li>
+    </ul>
+    <ul>
+      <li>
+        <a href="#">Item A</a>
+      </li>
+      <li>
+        <a href="#">Item B</a>
+      </li>
+    </ul>
+  </nav>
+</template>
+
+<script>
+</script>
